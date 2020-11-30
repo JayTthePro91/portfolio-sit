0d28de1d4be4a276cda6654e909f51682a85ee37
JayTthePro91 <59818702+JayTthePro91@users.noreply.github.com>
2020-11-30 17:59:50 -0500

    updated app.js and about me page

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -8,16 +8,19 @@ function AboutPage(props){
    return(
     <div>
         <Hero title={props.title}/>
-        <thecontent>
+       <thecontent>
        <p> Hey! I'm <strong>Juan</strong>. I am currently a computer science student at Lehman college and 
         I have a strong passion for Coding to the point where it has changed the way i 
         think and feel. It's not just about apps. Learning to code gave me <i>problem-solving skills</i>, 
         <i> critical thinkings skills</i>, and a way to communicate with others on a technical level. 
-        I am self taught html web developer but also have skills in java and python. 
+        I am self taught html css web developer but also have skills in java and python and I am currently studying javascipt with react. 
         In life there will always be a struggle and no matter what struggle or failure i encounter i will find a way to go around 
         it to keep succeeding and building up on my skills as a programmer and web developer.
        <b> There is no success until failure has been encountered</b></p>
-        </thecontent>
+       </thecontent>
+    
+    
+    
     </div>    
      
    );
